79c53388ae91352a631d10b0d7df4cb215fb0181
Rafael Dohms <rdohms@gmail.com>
2017-06-03 12:14:17 +0200

    Add test for container definition
    In order to check for support by all modern versions of symfony
    we run a simulated compilation of the container.

---
+++ Tests/Integration/ContainerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DMS\Bundle\TwigExtensionBundle\Tests\Integration;
+
+use DMS\Bundle\TwigExtensionBundle\DependencyInjection\DMSTwigExtensionExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class ContainerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    /**
+     * @before
+     * @return void
+     */
+    public function buildContainer()
+    {
+        $this->container = new ContainerBuilder();
+
+        // Cover external dependencies
+        $this->container->set('translator', $this->prophesize(TranslatorInterface::class)->reveal());
+
+        $extension = new DMSTwigExtensionExtension();
+        $extension->load([], $this->container);
+
+        $this->container->compile();
+    }
+
+    public function testContainerBoots()
+    {
+        $this->container->get('dms_twig_extension.dms.textual_date');
+        $this->container->get('dms_twig_extension.fabpot.array');
+
+        self::assertTrue($this->container->isCompiled());
+    }
+}
